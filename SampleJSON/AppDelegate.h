271ec656c68e73c682270fde72f4338de8d06796
ei17ringo <ei17ringo@gmail.com>
2015-04-20 10:13:24 +0800

    Initial Commit

---
+++ SampleJSON/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleJSON
+//
+//  Created by Eriko Ichinohe on 2015/04/20.
+//  Copyright (c) 2015年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
